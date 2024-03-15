c96114ae5799e6e6010663e65401ef08a6a7aef2
Mateus Sales Albino <mateus3009@gmail.com>
2024-03-15 07:47:07 -0300

    Feat: Creating API Gateway

---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "load_balancer_name" {
+  description = "The name of the load balancer"
+  type        = string
+}
+
+variable "target_group_port" {
+  description = "The port of the target group"
+  type        = number
+}
+
+variable "region" {
+  type    = string
+  default = "sa-east-1"
+}
+
